298b644fca0ba1b76abdbeec10670f19f0def61e
Kennie <the_reborn92@hotmail.com>
2016-05-24 16:28:52 +0200

    MyCrews - Name updated

--- app/src/main/java/com/example/buster/mycrews/UI/User/MyCrewsActivity.java
+++ app/src/main/java/com/example/buster/mycrews/UI/User/MyCrewsActivity.java
@@ -1,5 +1,8 @@
 package com.example.buster.mycrews.UI.User;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +20,11 @@ import com.example.buster.mycrews.InitializeTasks.InitializeTaskUserCrews;
 import com.example.buster.mycrews.MenuActivity;
 import com.example.buster.mycrews.R;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MyCrewsActivity extends MenuActivity {
@@ -24,11 +32,12 @@ public class MyCrewsActivity extends MenuActivity {
     LinearLayout crew1;
     TextView crewName1;
     ImageView crewImage1;
-
     LinearLayout crew2;
     TextView crewName2;
     ImageView crewImage2;
-
+    ArrayList<ImageView> imageViews = new ArrayList<>();
+    ArrayList<TextView> textViews = new ArrayList<>();
+    Bitmap mIcon_val;
     LinearLayout crew3;
     TextView crewName3;
     ImageView crewImage3;
@@ -47,6 +56,7 @@ public class MyCrewsActivity extends MenuActivity {
         crewManager = CrewManager.getInstance();
         loadMyCrews();
 
+
         crew1 = (LinearLayout) findViewById(R.id.crew1);
         crewName1 = (TextView) findViewById(R.id.crewName1);
         crewImage1 = (ImageView) findViewById(R.id.crewImage1);
@@ -60,6 +70,13 @@ public class MyCrewsActivity extends MenuActivity {
         crewImage3 = (ImageView) findViewById(R.id.crewImage3);
 
 
+        textViews.add(crewName1);
+        textViews.add(crewName2);
+        textViews.add(crewName3);
+
+        imageViews.add(crewImage1);
+        imageViews.add(crewImage2);
+        imageViews.add(crewImage3);
 
         crew1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,6 +111,11 @@ public class MyCrewsActivity extends MenuActivity {
     }
 
     public void instantiateUserCrews(ArrayList<Crew> crews) {
+        for (int i = 0; i < crews.size(); i++){
 
+
+                textViews.get(i).setText(crews.get(i).getCrewName());
+
+        }
     }
 }
