acf11f48ceafe8f2ac14c0b54c6bd3bf4a0851da
Tasina <tasin-a@hotmail.com>
2016-05-24 20:08:41 +0200

    CrewGames

--- app/src/main/java/com/example/buster/mycrews/UI/Crew/CrewGamesActivity.java
+++ app/src/main/java/com/example/buster/mycrews/UI/Crew/CrewGamesActivity.java
@@ -2,15 +2,32 @@ package com.example.buster.mycrews.UI.Crew;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.buster.mycrews.BE.CrewGameSuggestion;
 import com.example.buster.mycrews.MenuActivity;
 import com.example.buster.mycrews.R;
 
+import java.util.ArrayList;
+
 public class CrewGamesActivity extends MenuActivity {
+
+    ListView listView;
+
+    ArrayList<CrewGameSuggestion> gameSugs;
+    CrewGameSuggestionGameListViewAdapter crewGameSuggestionGameListViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crew_games);
+
+        listView = (ListView) findViewById(R.id.crewGameList);
+
+        gameSugs = new ArrayList<>();
+    }
+
+    public void populateGameList(){
+
     }
 }
